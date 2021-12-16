e2e220a0bc7580da7d594f7bcf2342e24e5a5250
gallego.gofra21 <gallego.gofra21@triana.salesianos.edu>
2021-12-16 08:47:37 +0100

    Firebase GasApp proteccion de rutas

--- FirebaseGasApp/src/app/modules/app-routing.module.ts
+++ FirebaseGasApp/src/app/modules/app-routing.module.ts
@@ -6,14 +6,16 @@ import { LoginComponent } from '../components/login/login.component';
 import { GasolineraListComponent } from '../components/gasolinera-list/gasolinera-list.component';
 import { GasolineraFavListComponent } from '../components/gasolinera-fav-list/gasolinera-fav-list.component';
 import { ListaListComponent } from '../components/lista-list/lista-list.component';
+import { AngularFireAuthGuard, loggedIn } from '@angular/fire/compat/auth-guard';
 
+const loggedInV = () => loggedIn;
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'login', pathMatch: 'full', redirectTo: '/login'},
-  { path: 'favorites', pathMatch: 'full', component: GasolineraFavListComponent},
-  { path: 'gasolineras', pathMatch: 'full', component: GasolineraListComponent},
-  { path: 'listas', pathMatch: 'full', component: ListaListComponent}
+  { path: 'favorites', pathMatch: 'full', component: GasolineraFavListComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: loggedInV} },
+  { path: 'gasolineras', pathMatch: 'full', component: GasolineraListComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: loggedInV} },
+  { path: 'listas', pathMatch: 'full', component: ListaListComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: loggedInV} }
   
 ];
 
