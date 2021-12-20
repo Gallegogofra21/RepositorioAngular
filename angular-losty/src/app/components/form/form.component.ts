606685422ade1ae78fb312671cbf9cdeae5d16ad
gallego.gofra21 <gallego.gofra21@triana.salesianos.edu>
2021-12-20 14:24:50 +0100

    Avances

--- angular-losty/src/app/components/form/form.component.ts
+++ angular-losty/src/app/components/form/form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ObjetoPerdidoDto } from 'src/app/models/interfaces/objeto-perdido.interface';
+import { ObjetoPerdidoService } from 'src/app/services/objeto-perdido.service';
+
+
+ 
 
 @Component({
   selector: 'app-form',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+
+  objetoPerdidoNuevo = new ObjetoPerdidoDto();
+
+  constructor(private objetoPerdidoService: ObjetoPerdidoService) { }
 
   ngOnInit(): void {
+  }
+
+  addNewObjetoPerdido() {
+    if(this.objetoPerdidoNuevo.name){
+      this.objetoPerdidoService.saveObjeto(this.objetoPerdidoNuevo?.name, this.objetoPerdidoNuevo?.descripcion, this.objetoPerdidoNuevo?.categoria, this.objetoPerdidoNuevo?.localizacion, this.objetoPerdidoNuevo)
+    }
   }
 
 }
